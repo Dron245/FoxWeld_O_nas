72fe779b9991a9c4e4d0cdbc4f267629799db2e2
Dron245 <Dron245@yandex.ru>
2023-06-20 21:31:23 +0300

    2006-2131

--- src/js/files/forms/inputmask.js
+++ src/js/files/forms/inputmask.js
@@ -2,12 +2,25 @@
 
 // Подключение функционала "Чертогов Фрилансера"
 // Подключение списка активных модулей
-import { flsModules } from "../modules.js";
+//import { flsModules } from "../modules.js";
 
 // Подключение модуля
 import "inputmask/dist/inputmask.min.js";
 
+// const inputMasks = document.querySelectorAll('input');
+// if (inputMasks.length) {
+// 	flsModules.inputmask = Inputmask().mask(inputMasks);
+// }
+
+
+
+import { flsModules } from "../modules.js";
+
+
 const inputMasks = document.querySelectorAll('input');
 if (inputMasks.length) {
+	
 	flsModules.inputmask = Inputmask().mask(inputMasks);
+	Inputmask({"mask": "+7(999) 999-9999"}).mask(document.querySelectorAll('.tel'));
+	//Inputmask({"mask": "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]"}).mask(document.querySelectorAll('.asd'));
 }
